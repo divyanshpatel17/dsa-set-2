4ba8a7cead3bebe0a19583232259a8c1b2943d8e
divyanshpatel17 <divyanshpatel1998@gmail.com>
2024-11-28 15:15:29 +0530

    s-2-q1.cpp

--- s-2-q1.cpp
+++ s-2-q1.cpp
@@ -24,3 +24,63 @@ int main() {
     cout << "Maximum subarray sum: " << maxSubArray(nums, size) << endl;
     return 0;
 }
+
+
+
+
+
+// Merge Sort Approach
+
+#include <iostream>
+using namespace std;
+
+int mergeAndCount(int arr[], int temp[], int left, int mid, int right) {
+    int i = left;  // Starting index for left subarray
+    int j = mid + 1;  // Starting index for right subarray
+    int k = left;  // Starting index to be sorted
+    int inv_count = 0;
+
+    while ((i <= mid) && (j <= right)) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+            inv_count += (mid - i + 1);  // Number of inversions
+        }
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+
+    return inv_count;
+}
+
+int mergeSortAndCount(int arr[], int temp[], int left, int right) {
+    int inv_count = 0;
+    if (left < right) {
+        int mid = (left + right) / 2;
+
+        inv_count += mergeSortAndCount(arr, temp, left, mid);
+        inv_count += mergeSortAndCount(arr, temp, mid + 1, right);
+        inv_count += mergeAndCount(arr, temp, left, mid, right);
+    }
+    return inv_count;
+}
+
+int countInversions(int arr[], int n) {
+    int temp[n];
+    return mergeSortAndCount(arr, temp, 0, n - 1);
+}
+
+int main() {
+    int arr[] = {7, 2, 6, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Number of inversions: " << countInversions(arr, n) << endl;
+    return 0;
+}
